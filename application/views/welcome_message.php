791f8e6b3d9f72de5e7cea8f754be9a3c96fdf2e
Rene Quisaguano <renequisaguano@gmail.com>
2014-09-01 15:55:33 -0500

    Sistemas de Notas
    Utilizando CodeIgniter

---
+++ application/views/welcome_message.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Sistema Esolastico</title>
+
+	
+</head>
+<body>
+
+
+	<center><h1>Bienvenidos al Sistema de Notas</h1></center>
+
+
+	
+</body>
+</html>
